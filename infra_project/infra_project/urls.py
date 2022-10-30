cb33544e3c388614a7074a695855c1e0c0e7beab
A.Sultanbekov <axa.sultan@mail.ru>
2022-10-30 19:54:13 +0600

    1.2

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -16,7 +16,3 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include('infra_app.urls', namespace='infra_app')),
-    path('admin/', admin.site.urls),
-]
